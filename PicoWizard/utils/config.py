3557ff3e0f7cf094895c94a8f9d83cea70463df7
Anupam Basak <anupam.basak27@gmail.com>
2022-12-09 17:31:23 +0530

    Allow overriding config values with custom config from /etc/pico-wizard/pico-wizard.conf.d/*.conf

--- PicoWizard/utils/config.py
+++ PicoWizard/utils/config.py
@@ -3,6 +3,7 @@
 
 import sys
 import configparser
+import pathlib
 from . import constants as conf
 from PicoWizard.utils.logger import Logger
 
@@ -17,10 +18,17 @@ class Config:
     def __getConfig__(config_section, config_name, fallback=''):
         if Config.config is None:
             Config.config = configparser.ConfigParser()
+            
+            # Read main config from /etc/pico-wizard/pico-wizard.conf
             read_files = Config.config.read(conf.CONFIG_FILE_PATH)
+            
             if len(read_files) == 0:
                 Config.log.error('Could not find or open config file ' + conf.CONFIG_FILE_PATH)
                 sys.exit(1)
+            else:
+                # Main config file read successfully.
+                # Read all custom configs from /etc/pico-wizard/pico-wizard.conf.d/*.conf (if any) and override values
+                Config.config.read(sorted(pathlib.Path(conf.CONFIG_FILE_PATH+".d").glob("*.conf")))
                 
         return Config.config[config_section].get(config_name, fallback=fallback)
 
